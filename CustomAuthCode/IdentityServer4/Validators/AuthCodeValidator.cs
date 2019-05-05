030977ec2b55160c8636d8d2178f43aea0782add
case651 <konstantin.akhmetov@gmail.com>
2019-05-05 23:08:29 +0300

    Add files via upload

---
+++ CustomAuthCode/IdentityServer4/Validators/AuthCodeValidator.cs
@@ -0,0 +1,37 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestIdentityServer4.Validators
+{
+    public class AuthCodeValidator : IExtensionGrantValidator
+    {
+        public string GrantType => "app2_auth_code";
+
+        public async Task ValidateAsync(ExtensionGrantValidationContext context)
+        {
+            var code = context.Request.Raw.Get("code");
+            var state = context.Request.Raw.Get("state");
+
+            var sub = GetSubByCode(code, state);
+
+            if (string.IsNullOrEmpty(sub))
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant);
+                return;
+            }
+            
+            context.Result = new GrantValidationResult(sub, GrantType);
+            return;
+        }
+
+        //Check the code and the state (and the request are still active) and returns sub
+        private string GetSubByCode(string code, string state)
+        {
+            return "tu1";
+        }
+    }
+}
---
+++ CustomAuthCode/app1/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace WebApp1
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Registering of identity server for web app1
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+                opt.DefaultAuthenticateScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+
+            })
+                .AddIdentityServerAuthentication(
+                    opt =>
+                    {
+                        opt.Authority = "http://localhost:5000";
+                        opt.RequireHttpsMetadata = false;
+                        opt.ApiName = "app1.api";
+                    });
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
